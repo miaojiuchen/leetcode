140d99c41e74b84e9b100749cd46f6a9a08f6890
mjcEatYou <305870677@qq.com>
2019-10-20 15:22:02 +0800

    N皇后

---
+++ Leetcode.Test/51 N皇后.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace Leetcode.Test
+{
+    class Test_51_N皇后
+    {
+        _51_N皇后 C;
+        [SetUp]
+        public void Setup()
+        {
+            C = new _51_N皇后();
+        }
+
+        [Test]
+        public void Test()
+        {
+            var x = C.SolveNQueens(4);
+            foreach (var xx in x)
+            {
+                Console.WriteLine(xx);
+            }
+        }
+    }
+}
---
+++ Leetcode/回溯算法/51 N皇后.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Leetcode
+{
+    public class _51_N皇后
+    {
+        private int n;
+        private int crossCount;
+        private int[] colCheck;
+        private bool[] rCrossCheck;
+        private bool[] lCrossCheck;
+
+        private List<IList<string>> ans;
+
+        public IList<IList<string>> SolveNQueens(int _n)
+        {
+            n = _n;
+            colCheck = new int[n];
+            for (var i = 0; i < n; i++)
+            {
+                colCheck[i] = -1;
+            }
+
+            crossCount = 2 * n - 1;
+            rCrossCheck = new bool[crossCount];
+            lCrossCheck = new bool[crossCount];
+
+            ans = new List<IList<string>>();
+            solve(0);
+            return ans;
+        }
+
+        private void solve(int row)
+        {
+            if (row == n)
+            {
+                ans.Add(BoardToString());
+                return;
+            }
+
+            for (var col = 0; col < n; col++)
+            {
+                if (checkAttack(row, col))
+                {
+                    continue;
+                }
+
+                colCheck[col] = row;
+                rCrossCheck[rCrossIndex(row, col)] = true;
+                lCrossCheck[lCrossIndex(row, col)] = true;
+                solve(row + 1);
+                colCheck[col] = -1;
+                rCrossCheck[rCrossIndex(row, col)] = false;
+                lCrossCheck[lCrossIndex(row, col)] = false;
+            }
+            return;
+        }
+
+        private bool checkAttack(int row, int col)
+        {
+            return colCheck[col] != -1 || rCrossCheck[rCrossIndex(row, col)] || lCrossCheck[lCrossIndex(row, col)];
+        }
+
+        // rCross 方程： x + y = crossIndex
+        private int rCrossIndex(int row, int col)
+        {
+            return row + col;
+        }
+
+        // lCross 方程： x - y = crossIndex - (crossCount 整除 2)
+        private int lCrossIndex(int row, int col)
+        {
+            return row - col + crossCount / 2;
+        }
+
+        private List<string> BoardToString()
+        {
+            var list = new List<string>();
+            for (var row = 0; row < n; row++)
+            {
+                StringBuilder sb = new StringBuilder();
+                for (var col = 0; col < n; col++)
+                {
+                    if (colCheck[col] == row)
+                    {
+                        sb.Append("Q");
+                    }
+                    else
+                    {
+                        sb.Append(".");
+                    }
+                }
+                list.Add(sb.ToString());
+            }
+            return list;
+        }
+    }
+}
