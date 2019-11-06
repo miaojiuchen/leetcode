a44e7dbfaec80332e7d2680bfd66fc0ff3c24f5e
mjcEatYou <305870677@qq.com>
2019-11-06 08:41:27 +0800

    LRU缓存

---
+++ Leetcode.Test/146 LRU缓存.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Leetcode.Test
+{
+    class Test_146_LRU缓存
+    {
+        _146_LRU缓存 c;
+
+
+        [Test]
+        public void Test()
+        {
+            var cache = new _146_LRU缓存.LRUCache(2);
+            var res = new int[5];
+            cache.Put(1, 1);
+            cache.Put(2, 2);
+            res[0] = cache.Get(1);       // 返回  1
+            cache.Put(3, 3);    // 该操作会使得密钥 2 作废
+            res[1] = cache.Get(2);       // 返回 -1 (未找到)
+            cache.Put(4, 4);    // 该操作会使得密钥 1 作废
+            res[2] = cache.Get(1);       // 返回 -1 (未找到)
+            res[3] = cache.Get(3);       // 返回  3
+            res[4] = cache.Get(4);       // 返回  4
+
+            var targets = new int[] { 1, -1, -1, 3, 4 };
+
+            if(res.Except(targets).Count() == 0)
+            {
+                Assert.Pass();
+            } else
+            {
+                Assert.Fail();
+            }
+        }
+    }
+}
---
+++ Leetcode/146 LRU缓存.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Leetcode
+{
+    public class _146_LRU缓存
+    {
+        public class LRUCache
+        {
+            public class Node
+            {
+                public int key;
+                public int val;
+                public Node prev, next;
+                public Node(int key, int val)
+                {
+                    this.key = key;
+                    this.val = val;
+                }
+            }
+
+            public class DLinkList
+            {
+                public Node head;
+                private Node tail;
+                public int count;
+
+                public void RemoveFront()
+                {
+                    head = head.next;
+                    if (head != null)
+                    {
+                        head.prev = null;
+                    }
+                    count--;
+                }
+
+                public void Remove(Node n)
+                {
+                    if (n.prev != null)
+                    {
+                        n.prev.next = n.next;
+                    }
+                    if (n.next != null)
+                    {
+                        n.next.prev = n.prev;
+                    }
+                    if (n == head)
+                    {
+                        head = n.next;
+                    }
+                    if (n == tail)
+                    {
+                        tail = n.prev;
+                    }
+                    n.prev = null;
+                    n.next = null;
+                    count--;
+                }
+
+                public void AddTail(Node n)
+                {
+                    if (count == 0)
+                    {
+                        head = n;
+                        tail = n;
+                    }
+                    else
+                    {
+                        n.prev = tail;
+                        tail.next = n;
+                        tail = n;
+                    }
+                    count++;
+                }
+            }
+
+            private DLinkList list;
+            private Dictionary<int, Node> dic;
+            private int _capacity;
+
+            public LRUCache(int capacity)
+            {
+                list = new DLinkList();
+                dic = new Dictionary<int, Node>();
+                _capacity = capacity;
+            }
+
+            public int Get(int key)
+            {
+                if (dic.ContainsKey(key))
+                {
+                    var node = dic[key];
+                    list.Remove(node);
+                    list.AddTail(node);
+                    return dic[key].val;
+                }
+                return -1;
+            }
+
+            public void Put(int key, int value)
+            {
+                if (dic.ContainsKey(key))
+                {
+                    list.Remove(dic[key]);
+                    var node = new Node(key, value);
+                    list.AddTail(node);
+                    dic[key] = node;
+                }
+                else
+                {
+                    if (list.count >= _capacity)
+                    {
+                        dic.Remove(list.head.key);
+                        list.RemoveFront();
+                    }
+                    var node = new Node(key, value);
+                    list.AddTail(node);
+                    dic.Add(key, node);
+                }
+            }
+        }
+    }
+}
+
